954fa84545a7a5d8ac5d57b51ac5e0583d62f2d2
frogBear_7 <{220098tt@yse-c.net}>
2023-10-19 16:25:26 +0900

    お手本動画選択ドロップダウンリストと初期値の場合画像を表示

--- kendodo-project/script/index.js
+++ kendodo-project/script/index.js
@@ -1,3 +1,4 @@
+// index.html
 // ページ読み込み時にアニメーションを開始
 window.addEventListener('load', function() {
     const background = document.queryselector('.background-image');
@@ -13,13 +14,4 @@ window.addEventListener('beforeunload', function() {
     background2.style.animationPlayState = 'running';
 });
 
-function imgChange(parts, id) {
-    /*選択されているメニューを変数へ格納*/
-    fname = parts.options[parts.selectedIndex].value;
-    /*選択された画像のパスを送る*/
-    if (fname == 0) id.src = "../style/images/kendo1.jpg";
-    if (fname == 1) id.src = "../style/svg/silhouette_gradation.svg";
-    if (fname == 2) id.src = "../style/images/overview-test.png";
-    if (fname == 3) id.src = "../style/images/kendo1.jpg";
-    if (fname == 4) id.src = "../style/svg/silhouette_gradation.svg";
-}
+
