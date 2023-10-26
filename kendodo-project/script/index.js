010524cf7548843de0f21d43e992609daca0e738
frogBear_7 <{220098tt@yse-c.net}>
2023-10-26 14:17:01 +0900

    index.js修正

--- kendodo-project/script/index.js
+++ kendodo-project/script/index.js
@@ -1,8 +1,8 @@
 // index.html
 // ページ読み込み時にアニメーションを開始
 window.addEventListener('load', function() {
-    const background = document.queryselector('.background-image');
-    const background2 = document.queryselector('.background-image2');
+    const background = document.querySelector('.background-image');
+    const background2 = document.querySelector('.background-image2');
     background.style.animationPlayState = 'running';
     background2.style.animationPlayState = 'running';
 });
