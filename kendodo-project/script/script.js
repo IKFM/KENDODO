b2a6395dd731fde2a49e8c073b555004f019da53
frogBear_7 <{220098tt@yse-c.net}>
2023-12-08 10:24:02 +0900

    up

--- kendodo-project/script/script.js
+++ kendodo-project/script/script.js
@@ -49,7 +49,7 @@ video1.addEventListener('ended', () => {
 const startButton = document.getElementById('start_button');
 let isProcessing = false;
 startButton.addEventListener('click', () => {
-    if (videoSelect.value === '0') { // お手本動画選択処理(仮)
+    if (videoSelect.value === '0') { // お手本動画選択処理
         alert('お手本動画を選択してください。');
         return; // ここで処理を終了
     }
@@ -62,7 +62,7 @@ startButton.addEventListener('click', () => {
 //停止ボタンの処理
 const stopButton = document.getElementById('stop_button');
 stopButton.addEventListener('click', () => {
-    if (videoSelect.value === '0') { // お手本動画選択処理(仮)
+    if (videoSelect.value === '0') { // お手本動画選択処理
         alert('お手本動画を選択してください。');
     } else {
         // お手本動画の停止処理
@@ -203,7 +203,7 @@ function calcAngleDegrees(x1, y1, x2, y2) {
 
 });
 
-// お手本動画選択処理(仮)
+// お手本動画選択処理
 document.addEventListener('DOMContentLoaded', async () => {
     // セレクトボックスとビデオ要素の取得
     const videoSelect = document.getElementById('videoSelect');
